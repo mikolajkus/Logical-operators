d0ec94112433f0469b607573b26a8c56b83b6655
mikolajkus <160539755+mikolajkus@users.noreply.github.com>
2024-04-03 23:17:37 +0200

    Added second exercise 'isFirstNumberIsCloseToSecondNumber'

--- script.js
+++ script.js
@@ -8,3 +8,33 @@ function isSetAlarm(isEmployed, isOnAVacation){
 }
 
 console.log(isSetAlarm(true, false));
+
+// 2. Create a function close_compare that accepts 3 parameters: a, b, and an optional margin.
+// The function should return whether a is lower than, close to, or higher than b.
+// Please note the following:
+// When a is close to b, return 0.
+// For this challenge, a is considered "close to" b if margin is greater than or equal to the absolute distance between a and b.
+// Otherwise...
+// When a is less than b, return -1.
+// When a is greater than b, return 1.
+// If margin is not given, treat it as if it were zero.
+// Assume: margin >= 0
+// Tip: Some languages have a way to make parameters optional.
+
+function isFirstNumberIsCloseToSecondNumber(firstNumber, secondNumber, marginParameter) {
+    if (marginParameter === undefined) {
+        marginParameter = 0;
+    }
+    const absoluteValue = Math.abs(firstNumber - secondNumber);
+    if (absoluteValue <= marginParameter) {
+        return 0;
+    }
+    if (firstNumber < secondNumber) {
+        return -1;
+    }
+    if (firstNumber > secondNumber) {
+        return 1;
+    }
+}
+
+console.log(isFirstNumberIsCloseToSecondNumber(9,5, 0));
