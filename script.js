a52ffb285e327e051216aa3647e928fb7ed6b9b3
mikolajkus <160539755+mikolajkus@users.noreply.github.com>
2024-04-06 12:06:46 +0200

    Modified the variable name from absoluteVlaueOfNumber to absoluteDistanceBetweenNumbers

--- script.js
+++ script.js
@@ -22,8 +22,8 @@ console.log(isAlarmSet(true, false));
 // Tip: Some languages have a way to make parameters optional.
 
 function isTheFirstNumberCloseToTheSecondNumber(firstNumber, secondNumber, marginParameter = 0) {
-    const absoluteValueOfNumbers = Math.abs(firstNumber - secondNumber);
-    if (absoluteValueOfNumbers <= marginParameter) {
+    const absoluteDistanceBetweenNumbers = Math.abs(firstNumber - secondNumber);
+    if (absoluteDistanceBetweenNumbers <= marginParameter) {
         return 0;
     }
     if (firstNumber < secondNumber) {
