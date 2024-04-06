e574fbf3028dfee77d91d71362ace35b3d58642e
mikolajkus <160539755+mikolajkus@users.noreply.github.com>
2024-04-06 14:22:12 +0200

    Added the exercise number 7 to separate js file

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,31 +1,41 @@
 // 7. In this kata, your task is to implement an extended version of the famous rock-paper-scissors game.
 
-function willRockWin(playerTwo) {
-    return playerTwo === 'scissors' || playerTwo === 'lizard';
+function willRockWin(player) {
+    return player === 'scissors' || player === 'lizard';
 }
 
-function willScissorsWin(playerTwo) {
-    return playerTwo === 'paper' || playerTwo === 'lizard';
+function willScissorsWin(player) {
+    return player === 'paper' || player === 'lizard';
 }
 
-function willLizardWin(playerTwo) {
-    return playerTwo === 'paper' || playerTwo === 'spock';
-
-}
-function willPaperWin(playerTwo) {
-    return playerTwo === 'spock' || playerTwo === 'rock';
+function willLizardWin(player) {
+    return player === 'paper' || player === 'spock';
 }
 
-function willSpockWin(playerTwo) {
-    return playerTwo === 'scissors' || playerTwo === 'rock';
+function willPaperWin(player) {
+    return player === 'spock' || player === 'rock';
+}
+
+function willSpockWin(player) {
+    return player === 'scissors' || player === 'rock';
 }
 
 function extendedVersionOfRockPaperScissors(playerOne, playerTwo){
+    const rockWin = willRockWin(playerTwo);
+    const scissorsWin = willScissorsWin(playerTwo);
+    const lizardWin = willLizardWin(playerTwo);
+    const paperWin = willPaperWin(playerTwo);
+    const spockWin = willSpockWin(playerTwo);
     if (playerOne === playerTwo) {
         return 'Draw!';
     }
-    if (willRockWin(playerTwo))
-     {
+    if (
+        (playerOne && rockWin) ||
+        (playerOne && scissorsWin) ||
+        (playerOne && lizardWin) ||
+        (playerOne && paperWin) ||
+        (playerOne && spockWin)
+    ) {
         return 'Player 1 Won!';
     }
     return 'Player 2 Won!';
