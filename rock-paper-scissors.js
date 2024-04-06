1f37e558ab9258b16bb403a206856f772b0083b8
mikolajkus <160539755+mikolajkus@users.noreply.github.com>
2024-04-06 13:18:56 +0200

    Added the rock-paper-scissors js file

---
+++ rock-paper-scissors.js
@@ -0,0 +1,38 @@
+// 7. In this kata, your task is to implement an extended version of the famous rock-paper-scissors game.
+
+function willRockWin(playerTwo) {
+    return playerTwo === 'scissors' || playerTwo === 'lizard';
+}
+
+function willScissorsWin(playerTwo) {
+    return playerTwo === 'paper' || playerTwo === 'lizard';
+}
+
+function willLizardWin(playerTwo) {
+    return playerTwo === 'paper' || playerTwo === 'spock';
+
+}
+function willPaperWin(playerTwo) {
+    return playerTwo === 'spock' || playerTwo === 'rock';
+}
+
+function willSpockWin(playerTwo) {
+    return playerTwo === 'scissors' || playerTwo === 'rock';
+}
+
+function extendedVersionOfRockPaperScissors(playerOne, playerTwo){
+    if (playerOne === playerTwo) {
+        return 'Draw!';
+    }
+    if (willRockWin(playerTwo) ||
+        willScissorsWin(playerTwo) ||
+        willLizardWin() ||
+        willPaperWin() ||
+        willSpockWin()
+    ) {
+        return 'Player 1 Won!';
+    }
+    return 'Player 2 Won!';
+}
+
+console.log(extendedVersionOfRockPaperScissors('rock', 'paper'));
